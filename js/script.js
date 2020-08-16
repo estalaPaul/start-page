cd6416ad1e062b9a6637ff0875b77aaf5a5b1d85
Paul Estala <32235423+estalaPaul@users.noreply.github.com>
2020-08-16 13:51:09 -0600

    feat: redesignnnnnn

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
-var btnText;
-
-$(document).ready(function(){
-    $(".bookmarks").hover(function(){
-      $(".bookmarks").animate({'left':"0px"}, 500)
-      $("#btn").text("<<");
-    },
-    function(){
-      $(".bookmarks").animate({'left':"-170px"}, 500)
-      $("#btn").text(">>");
-    });
+document.addEventListener('keydown', function (e) {
+    const searchInput = document.getElementById('search-input')
+    if (document.activeElement !== searchInput) {
+        const searchForm = document.getElementById('search-form');
+        if (e.key == 'g') {
+            searchForm.action = 'https://www.google.com/search';
+            searchInput.style.borderColor = '#3d7ff0';
+        } else if (e.key == 'd') {
+            searchForm.action = 'https://www.duckduckgo.com/';
+            searchInput.style.borderColor = '#d75534';
+        }
+    }
 });
